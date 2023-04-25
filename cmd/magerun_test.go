5b82107be816ccf9534d20b11b90cbf8f2b012ad
Francis Gallagher <francis917@gmail.com>
2023-04-25 21:49:25 +0100

    fix: Correct tests to work with new functionality

--- cmd/magerun_test.go
+++ cmd/magerun_test.go
@@ -1,54 +1,53 @@
 package cmd
 
 import (
-	"magecomm/messages/publisher"
-	"strings"
-	"testing"
+    "magecomm/messages/publisher"
+    "strings"
+    "testing"
 
-	"github.com/stretchr/testify/assert"
+    "github.com/stretchr/testify/assert"
 )
 
 type testMessagePublisher struct {
-	Queue            string
-	MessageBody      string
-	AddCorrelationID string
+    Queue            string
+    MessageBody      string
+    AddCorrelationID string
 }
 
-func (t *testMessagePublisher) Publish(queue string, messageBody string, AddCorrelationID string) (string, error) {
-	t.Queue = queue
-	t.MessageBody = messageBody
-	t.AddCorrelationID = AddCorrelationID
-	return "UniqueCorrelationID", nil
+func (t *testMessagePublisher) Publish(messageBody string, queue string, AddCorrelationID string) (string, error) {
+    t.Queue = queue
+    t.MessageBody = messageBody
+    t.AddCorrelationID = AddCorrelationID
+    return "UniqueCorrelationID", nil
 }
 
 func TestMagerunCmd(t *testing.T) {
-	testPublisher := &testMessagePublisher{}
-	publisher.SetMessagePublisher(testPublisher)
+    testPublisher := &testMessagePublisher{}
+    publisher.SetMessagePublisher(testPublisher)
 
-	testRootCmd := CreateTestRootCmd()
-	testRootCmd.AddCommand(MagerunCmd)
-	testArgs := []string{"magerun", "cache:clean"}
-	testRootCmd.SetArgs(testArgs)
-	err := testRootCmd.Execute()
+    testRootCmd := CreateTestRootCmd()
+    testRootCmd.AddCommand(MagerunCmd)
+    testArgs := []string{"magerun", "cache:clean"}
+    testRootCmd.SetArgs(testArgs)
+    _ = testRootCmd.Execute()
 
-	assert.NoError(t, err)
-	assert.Equal(t, MageRunQueue, testPublisher.Queue)
-	assert.Equal(t, "cache:clean", testPublisher.MessageBody)
+    assert.Equal(t, MageRunQueue, testPublisher.Queue)
+    assert.Equal(t, "cache:clean", testPublisher.MessageBody)
 
-	publisher.SetMessagePublisher(&publisher.DefaultMessagePublisher{})
+    publisher.SetMessagePublisher(&publisher.DefaultMessagePublisher{})
 }
 
 func TestMagerunCmdBlocksBannedCmd(t *testing.T) {
-	testPublisher := &testMessagePublisher{}
-	publisher.SetMessagePublisher(testPublisher)
+    testPublisher := &testMessagePublisher{}
+    publisher.SetMessagePublisher(testPublisher)
 
-	testRootCmd := CreateTestRootCmd()
-	testRootCmd.AddCommand(MagerunCmd)
-	testArgs := []string{"magerun", "module:enable", "Vendor_Module"}
-	testRootCmd.SetArgs(testArgs)
-	err := testRootCmd.Execute()
+    testRootCmd := CreateTestRootCmd()
+    testRootCmd.AddCommand(MagerunCmd)
+    testArgs := []string{"magerun", "module:enable", "Vendor_Module"}
+    testRootCmd.SetArgs(testArgs)
+    err := testRootCmd.Execute()
 
-	assert.True(t, strings.Contains(err.Error(), "the command 'module:enable' is not allowed"))
+    assert.True(t, strings.Contains(err.Error(), "the command 'module:enable' is not allowed"))
 
-	publisher.SetMessagePublisher(&publisher.DefaultMessagePublisher{})
+    publisher.SetMessagePublisher(&publisher.DefaultMessagePublisher{})
 }
